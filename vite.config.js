b61726139294809361dd3fe299591a253fee7ea5
Shariq Ahmad <shariqahmad303@gmail.com>
2026-09-20 10:15:28 +0530

    Add PWA support and update portfolio logo

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
+     VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Shariq Ahmad - Portfolio",
+        short_name: "Shariq Portfolio",
+        description: "Personal portfolio of Shariq Ahmad",
+        theme_color: "#06b6d4",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 })
